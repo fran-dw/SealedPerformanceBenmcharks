39a396eed64e96d8d414509de6cf51ed882873dc
Fran <fwahl@oneviewhealthcare.com>
2022-09-26 16:50:48 +0100

    initial code and benchmarks

---
+++ SealedPerformance/Benchmarks.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace SealedPerformance
+{
+    [MemoryDiagnoser(false)]
+    public class Benchmarks
+    {
+        private readonly BaseClass _baseClass = new();
+        private readonly OpenClass _openClass = new();
+        private readonly SealedClass _sealedClass = new();
+
+        private readonly OpenClass[] _openClasses = new OpenClass[1];
+        private readonly SealedClass[] _sealedClasses = new SealedClass[1];
+
+            [Benchmark]
+        public void OpenVoid() => _openClass.VoidMethod();
+        
+        [Benchmark]
+        public void SealedVoid() => _sealedClass.VoidMethod();
+        
+        [Benchmark]
+        public int OpenInt() => _openClass.IntMethod() + 9999;
+        
+        [Benchmark]
+        public int SealedInt() => _sealedClass.IntMethod() + 9999;
+
+        [Benchmark]
+        public bool IsCheckOpen() => _baseClass is OpenClass;
+        
+        [Benchmark]
+        public bool IsCheckSealed() => _baseClass is SealedClass;
+
+        [Benchmark]
+        public void StoreOpen() => _openClasses[0] = _openClass;
+        
+        [Benchmark]
+        public void StoreSealed() => _sealedClasses[0] = _sealedClass;
+    }
+}
---
+++ SealedPerformance/SampleClasses.cs
@@ -0,0 +1,23 @@
+﻿namespace SealedPerformance
+{
+    public class BaseClass
+    {
+        public virtual void VoidMethod() { }
+
+        public virtual int IntMethod() => 0;
+    }
+
+    public class OpenClass : BaseClass
+    {
+        public override void VoidMethod() { }
+
+        public override int IntMethod() => 10;
+    }
+
+    public sealed class SealedClass : BaseClass
+    {
+        public override void VoidMethod() { }
+
+        public override int IntMethod() => 20;
+    }
+}
